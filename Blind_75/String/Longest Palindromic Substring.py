2fa261284ce51b1e7e095942d9a29d77fc817c35
Hansen <hansenli@usc.edu>
2023-08-14 02:31:34 -0700

    update

--- Blind_75/String/Longest Palindromic Substring.py
+++ Blind_75/String/Longest Palindromic Substring.py
@@ -5,4 +5,22 @@
 
 # reverse the string and find the longest common subsequence
 
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        return self.longest_common_str(s, s[::-1])
 
+    def longest_common_str(self, s1, s2):
+        m = len(s1) + 1
+        n = len(s2) + 1
+
+        dp = [[0 for i in range(n)] for j in range(m)]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[m - 1][n - 1]
